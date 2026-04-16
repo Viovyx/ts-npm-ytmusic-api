7800019c608096feeced93f7ffd64c329f82be68
Renzo Rouma <renzo.rouma@gmail.com>
2026-04-16 02:40:45 +0200

    append highres thumbnail to thumbnail list

--- src/utils/traverse.ts
+++ src/utils/traverse.ts
@@ -1,38 +1,49 @@
+import getHighresThumbnail from "./getHighresThumbnail"
+
 export const traverse = (data: any, ...keys: string[]) => {
-	const again = (data: any, key: string, deadEnd = false): any => {
-		const res = []
+    const again = (data: any, key: string, deadEnd = false): any => {
+        const res = []
 
-		if (data instanceof Object && key in data) {
-			res.push(data[key])
-			if (deadEnd) return res.length === 1 ? res[0] : res
-		}
+        if (data instanceof Object && key in data) {
+            res.push(data[key])
+            if (deadEnd) return res.length === 1 ? res[0] : res
+        }
 
-		if (data instanceof Array) {
-			res.push(...data.map(v => again(v, key)).flat())
-		} else if (data instanceof Object) {
-			res.push(
-				...Object.keys(data)
-					.map(k => again(data[k], key))
-					.flat(),
-			)
-		}
+        if (data instanceof Array) {
+            res.push(...data.map((v) => again(v, key)).flat())
+        } else if (data instanceof Object) {
+            res.push(
+                ...Object.keys(data)
+                    .map((k) => again(data[k], key))
+                    .flat(),
+            )
+        }
 
-		return res.length === 1 ? res[0] : res
-	}
+        return res.length === 1 ? res[0] : res
+    }
 
-	let value = data
-	const lastKey = keys.at(-1)
-	for (const key of keys) {
-		value = again(value, key, lastKey === key)
-	}
+    let value = data
+    const lastKey = keys.at(-1)
+    for (const key of keys) {
+        value = again(value, key, lastKey === key)
+    }
+    if (lastKey == "thumbnails") {
+        try {
+            const highresThumbnail = getHighresThumbnail(value)
+            if (highresThumbnail != null) value.push(highresThumbnail)
+        } catch (error) {
+            // Something went wrong pushing the highres thumbnail
+            // console.error(error)
+        }
+    }
 
-	return value
+    return value
 }
 
 export const traverseList = (data: any, ...keys: string[]): any[] => {
-	return [traverse(data, ...keys)].flat()
+    return [traverse(data, ...keys)].flat()
 }
 
 export const traverseString = (data: any, ...keys: string[]): string => {
-	return traverseList(data, ...keys).at(0) || ""
+    return traverseList(data, ...keys).at(0) || ""
 }
